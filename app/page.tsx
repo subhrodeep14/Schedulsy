47103158cdc9a5f614b95b655298213b884c3649
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-08-06 23:23:37 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div>
       i am subhrodeep acharya . i will build this app in next 10 days and start my saas journey.
+      <button>i will</button>
     </div>
   );
 }
